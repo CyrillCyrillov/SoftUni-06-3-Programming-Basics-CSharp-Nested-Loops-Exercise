4c67b9e158ea30bb2973d375666d1e10dba2c170
Kiril Kirilov <kirilkirilov@abv.bg>
2021-01-07 20:48:44 +0200

    Add files via upload

---
+++ Task03_Sum_Prime_Non_Prime/Program.cs
@@ -0,0 +1,46 @@
+﻿using System;
+
+namespace Task03_Sum_Prime_Non_Prime
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int sumPrime = 0;
+            int sumNonPrime = 0;
+            string nextNumber = " ";
+            int check = 0;
+
+            
+            while (true)
+            {
+                nextNumber = (Console.ReadLine());
+                if (nextNumber == "stop") break;
+                int nextNumberDigit = int.Parse(nextNumber);
+                if (nextNumberDigit < 0)
+                {
+                    Console.WriteLine("Number is negative.");
+                }
+                else 
+                {
+                    check = 0;
+                    for (int i = 2; i < (nextNumberDigit / 2); i++)
+                    {
+                        if (nextNumberDigit % i == 0)
+                        {
+                            sumNonPrime += nextNumberDigit;
+                            check = 1;
+                            break;
+                        }
+                    }
+                    if (check == 0)
+                    {
+                        sumPrime += nextNumberDigit;
+                    }
+                }
+            }
+            Console.WriteLine($"Sum of all prime numbers is: {sumPrime}");
+            Console.WriteLine($"Sum of all non prime numbers is: {sumNonPrime}");
+        }
+    }
+}
