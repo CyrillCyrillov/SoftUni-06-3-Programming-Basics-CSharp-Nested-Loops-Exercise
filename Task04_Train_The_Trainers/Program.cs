80f9cfe7baecb438fefc3725643b465178298b8e
Kiril Kirilov <kirilkirilov@abv.bg>
2021-01-07 20:49:44 +0200

    Add files via upload

---
+++ Task04_Train_The_Trainers/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+namespace Task04_Train_The_Trainers
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string nextTheme = " ";
+            int numbersOfJudjs = int.Parse(Console.ReadLine());
+            
+            int count = 0;
+            double globalSum = 0;
+            while (true)
+            {
+                nextTheme = Console.ReadLine();
+                if (nextTheme == "Finish") break;
+                double helpSum = 0;
+                for (int i = 1; i <= numbersOfJudjs; i++)
+                {
+                    double nextEvaluation = double.Parse(Console.ReadLine());
+                    helpSum += nextEvaluation;
+                }
+                Console.WriteLine($"{nextTheme} - {(helpSum / numbersOfJudjs):F2}.");
+                count += 1;
+                globalSum += (helpSum / numbersOfJudjs);
+            }
+            Console.WriteLine($"Student's final assessment is {(globalSum / count):F2}.");
+        }
+    }
+}
