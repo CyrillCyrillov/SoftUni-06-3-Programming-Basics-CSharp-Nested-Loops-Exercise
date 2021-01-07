2efe16ada60c985b5107ba77e01277426ee9a218
Kiril Kirilov <kirilkirilov@abv.bg>
2021-01-07 20:46:33 +0200

    Add files via upload

---
+++ Task01_Number_Pyramid/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace Task01_Number_Pyramid
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+            int countPrint = 1;
+            int contForNewLine = 0;
+            for (int i = 1; i <= n; i++)
+            {
+                while (contForNewLine != i)
+                {
+                    if (countPrint == n+1) break;
+                    Console.Write($"{countPrint} ");
+                    countPrint += 1;
+                    contForNewLine += 1;
+                    if (contForNewLine == i) break;
+                }
+                contForNewLine = 0;
+                Console.WriteLine();
+            }
+        }
+    }
+}
