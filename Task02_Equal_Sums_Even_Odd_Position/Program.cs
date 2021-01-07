e539030c30c5dad0a5051ff24f2a2450b959feb4
Kiril Kirilov <kirilkirilov@abv.bg>
2021-01-07 20:47:43 +0200

    Add files via upload

---
+++ Task02_Equal_Sums_Even_Odd_Position/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+
+namespace Task02_Equal_Sums_Even_Odd_Position
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int firstNumber = int.Parse(Console.ReadLine());
+            int secondNumber = int.Parse(Console.ReadLine());
+            for (int i = firstNumber; i <= secondNumber; i++)
+            {
+                int evenSum = 0;
+                int oddSum = 0;
+
+                string curentNumber = i.ToString();
+                for (int j = 0; j < curentNumber.Length; j++)
+                {
+                    int curentDigit = int.Parse(curentNumber[j].ToString());
+                    if (j % 2 == 0)
+                    {
+                        evenSum += curentDigit;
+                    }
+                    else
+                    {
+                        oddSum += curentDigit;
+                    }
+                }
+                if (oddSum == evenSum)
+                {
+                    Console.Write($"{i} ");
+                }
+            }
+        }
+    }
+}
